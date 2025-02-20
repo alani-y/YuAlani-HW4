8eff8a181e634dd77bbbd49777b6ca5b1d0b8a41
Alani Yu <alani.yu79@utexas.edu>
2025-02-19 19:04:02 -0600

    added pip value screen

---
+++ YuAlani-HW4/PipVC.swift
@@ -0,0 +1,29 @@
+//
+//  PipVC.swift
+//  YuAlani-HW4
+//
+//  Created by Alani Yu on 2/19/25.
+//
+
+import UIKit
+
+class PipVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

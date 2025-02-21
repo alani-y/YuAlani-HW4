e4a87a9281cc7a240a33a4d78025212781ca13a0
Alani Yu <alani.yu79@gmail.com>
2025-02-21 11:52:12 -0600

    added guess functionality

--- YuAlani-HW4/ViewController.swift
+++ YuAlani-HW4/ViewController.swift
@@ -18,22 +18,22 @@ class ViewController: UIViewController {
     
     var guessNum = 0
     
+    let correctPip:String = pipValues[Int.random(in: 0...(pipValues.count-1))]
+    let correctSuit:String = suits[Int.random(in: 0...(suits.count-1))]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // generates a random pip and suit
-        let correctPip = pipValues[Int.random(in: 0...(pipValues.count-1))]
-        let correctSuit = suits[Int.random(in: 0...(suits.count-1))]
         
         print(correctPip)
         print(correctSuit)
         
-        // makes all the
+        // makes all the status labels invisible at the start
         pipStatus.alpha = 0
         suitStatus.alpha = 0
         guessStatus.alpha = 0
     }
 
+    // generates a random pip and suit
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
@@ -45,13 +45,33 @@ class ViewController: UIViewController {
     // checks if the user guessed the pip and suit
     @IBAction func submitButton(_ sender: Any) {
         
+        // makes the status visible
+        guessStatus.alpha = 1
+        
+        // checks if the user made a valid guess
         if((userPip != "Select Pip") && (userSuit != "Select Suit"))
         {
             guessNum += 1
+            
+            let userGuess = pipValues.firstIndex(of: userPip) ?? -1
+            let correctGuess = pipValues.firstIndex(of: correctPip) ?? -1
+            
+            if(userGuess < correctGuess)
+            {
+                guessStatus.text = "Your pip value is too low."
+            }
+            else if(userGuess > correctGuess)
+            {
+                guessStatus.text = "Your pip value is too high."
+            }
+            else
+            {
+                guessStatus.text = "Correctly guessed!"
+            }
         }
         else
         {
-            
+            guessStatus.text = "Select a pip value and suit first!"
         }
         
     }
